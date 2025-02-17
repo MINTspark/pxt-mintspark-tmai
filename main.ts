03205cf4b77c65105678ec25ac8534968ba6bb94
MINTspark <info@mintspark.io>
2025-02-17 10:31:56 +0100

    Aktualisiere pxt.json, main.ts

--- main.ts
+++ main.ts
@@ -1,1 +1,24 @@
 
+let index = 0
+let tempClassPrediction = ""
+let tempClassName = ""
+let serialParts: string[] = []
+let SerialData = ""
+let ClassName: string[] = []
+let ClassPrediction: number[] = []
+ClassPrediction = []
+serial.redirectToUSB()
+
+serial.onDataReceived(serial.delimiters(Delimiters.NewLine), function () {
+    SerialData = serial.readUntil(serial.delimiters(Delimiters.NewLine))
+    serialParts = SerialData.split(":")
+    tempClassName = serialParts[0]
+    tempClassPrediction = serialParts[1]
+    index = ClassName.indexOf(tempClassName)
+    if (index == -1) {
+        ClassName.push(tempClassName)
+        ClassPrediction.push(parseFloat(tempClassPrediction))
+    } else {
+        ClassPrediction[index] = parseFloat(tempClassPrediction)
+    }
+})
